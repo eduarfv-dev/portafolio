56928adb00e03116511712a24b8451747ef965d5
Eduardo <eduar123pc@gmail.com>
2025-06-04 11:11:30 -0500

    Configure Next.js for static export

--- mi-portafolio/next.config.ts
+++ mi-portafolio/next.config.ts
@@ -1,7 +1,10 @@
+// mi-portafolio/next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // config options here
+  output: 'export', 
 };
 
 export default nextConfig;
